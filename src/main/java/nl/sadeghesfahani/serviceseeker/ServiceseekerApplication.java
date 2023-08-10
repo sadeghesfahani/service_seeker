35cdf00d11a695ecf313d6cf3c43ad27a0b008aa
Sina <sadeghesfahani.sina@gmail.com>
2023-08-10 20:39:24 +0200

    initial commit

---
+++ src/main/java/nl/sadeghesfahani/serviceseeker/ServiceseekerApplication.java
@@ -0,0 +1,13 @@
+package nl.sadeghesfahani.serviceseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceseekerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceseekerApplication.class, args);
+    }
+
+}
